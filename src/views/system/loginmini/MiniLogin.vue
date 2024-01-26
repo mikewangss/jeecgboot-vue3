897e48f1b6012108bbb0c9a7bf323ca476f28841
Carbon <oncwnuKcx5WQRJ7xJRevRrFLUo50@git.weixin.qq.com>
2024-01-26 11:32:39 +0800

    删除jeecg_ad_text图片提交

--- src/views/system/loginmini/MiniLogin.vue
+++ src/views/system/loginmini/MiniLogin.vue
@@ -18,7 +18,7 @@
           <div class="aui-form">
             <div class="aui-image">
               <div class="aui-image-text">
-                <img :src="adTextImg" />
+<!--                <img :src="adTextImg" />-->
               </div>
             </div>
             <div class="aui-formBox">
@@ -161,7 +161,7 @@
   import MiniRegister from './MiniRegister.vue';
   import MiniCodelogin from './MiniCodelogin.vue';
   import logoImg from '/@/assets/loginmini/icon/jeecg_logo.png';
-  import adTextImg from '/@/assets/loginmini/icon/jeecg_ad_text.png';
+  // import adTextImg from '/@/assets/loginmini/icon/jeecg_ad_text.png';
   import { AppLocalePicker, AppDarkModeToggle } from '/@/components/Application';
   import { useLocaleStore } from '/@/store/modules/locale';
   import { useDesign } from '/@/hooks/web/useDesign';
